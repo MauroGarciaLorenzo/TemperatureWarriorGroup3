2c630798d687a2c15617a330fbe66b1007e704b1
davra02 <davidreyesales@gmail.com>
2026-01-15 13:07:16 +0100

    Conexion iphone David

--- TemperatureWarriorCode/Secrets.cs
+++ TemperatureWarriorCode/Secrets.cs
@@ -2,9 +2,9 @@
     internal class Secrets {
 
         //WiFi NAME 
-        public const string WIFI_NAME = "Galaxy S23 0637";
+        public const string WIFI_NAME = "iPhone de David";
 
         //PASSWORD FOR THE WiFi NAME
-        public const string WIFI_PASSWORD = "zs9avaszmdj4y5j";
+        public const string WIFI_PASSWORD = "Deterjuli123#";
     }
 }
